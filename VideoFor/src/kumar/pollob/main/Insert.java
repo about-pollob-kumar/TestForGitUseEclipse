d2ef4671a7a02a1f5f4d4a8a7f4b26e3b392fea9
pollo <pollo@POLLOB>
2025-03-17 19:13:24 +0600

    now this time i'm commit using eclipse. this local project name is "VideoFor"

---
+++ VideoFor/src/kumar/pollob/main/Insert.java
@@ -0,0 +1,36 @@
+package kumar.pollob.main;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class Insert {
+	
+	public static void main(String []args) throws ClassNotFoundException, SQLException {
+		
+		Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection 	c =DriverManager.getConnection("jdbc:mysql://localhost:3306/Pollob", "root", "Pollob@");
+        PreparedStatement p=c.prepareStatement("INSERT INTO PUPC VALUES(?,?,?)");
+           p.setString(1, "fjffjf");
+           p.setString(2, "01714..91" );
+           p.setString(3, "pollob@gmail.com");
+    
+    
+    
+        int e=p.executeUpdate();
+        
+        if(e>0) {
+        	System.out.println("insert success");
+        }
+        
+        else {
+        	System.out.println("fail");
+        }
+        
+        
+        c.close();
+        p.close();
+	}
+
+}
